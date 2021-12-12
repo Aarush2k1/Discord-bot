f0dd9399ef15b589fd0e48f4e1257a72d5ac0a6b
aarukansal2k1 <aarukansal2k1@gmail.com>
2021-12-12 19:30:43 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+require("dotenv").config();
+
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const prefix = "$";
+
+client.on("message", async (msg) => {
+  // if author bot, don't send any message
+  if (msg.author.bot) return;
+
+  // if msg starts with prefix, store commans in arry
+  if (msg.content.startsWith(prefix)) {
+    const [commandName, ...args] = msg.content.trim().substring().split(/\s+/);
+
+    switch (cmdName) {
+      case "$coa":
+        sendDocuments("coa", args, msg);
+        break;
+      case "$ps":
+        sendDocuments("pands", args, msg);
+        break;
+      case "$ml":
+        sendDocuments("ml", args, msg);
+        break;
+      case "$se":
+        sendDocuments("se", args, msg);
+        break;
+      case "$ele":
+        sendDocuments("ele", args, msg);
+        break;
+      default:
+        msg.reply("Commands are:\n $coa");
+    }
+  }
+});
+
+function sendDocuments(name, args, msg) {
+  if (args == "") {
+    fetchsubject(name);
+    msg.reply(name + " resources are:");
+  } else {
+    if (args == "syllabus") {
+      console.log("syallbus");
+    }
+  }
+}
+
+client.login(process.env.TOKEN);
